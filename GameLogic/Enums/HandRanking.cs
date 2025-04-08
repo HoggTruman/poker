a7a7fe00e8d0490b1b95aa09a067db35154e8f3e
Truman Hogg <hoggtruman@gmail.com>
2025-04-08 16:35:54 +0100

    Add HandRanking

---
+++ GameLogic/Enums/HandRanking.cs
@@ -0,0 +1,14 @@
+﻿namespace GameLogic.Enums;
+
+public enum HandRanking
+{
+    HighCard = 1,
+    Pair,
+    TwoPairs,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush
+}
