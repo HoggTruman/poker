aaa2887aa142614e765cc973fa7c511fdf46a148
Truman Hogg <hoggtruman@gmail.com>
2025-04-08 17:23:16 +0100

    Add Card

---
+++ GameLogic/Card.cs
@@ -0,0 +1,9 @@
+﻿using GameLogic.Enums;
+
+namespace GameLogic;
+
+public readonly struct Card(CardValue value, CardSuit suit)
+{
+    public readonly CardValue Value = value;
+    public readonly CardSuit Suit = suit;
+}
