6eddbe3f795570d1b92787f7712feec698e2553f
Truman Hogg <hoggtruman@gmail.com>
2025-04-08 20:43:27 +0100

    Add TieBreaker Tests

--- GameLogicTests/HandTests.cs
+++ GameLogicTests/HandTests.cs
@@ -295,4 +295,385 @@ public class HandTests
     #endregion
 
 
+    #region CompareWith Tests Without Tiebreakers
+
+    [Fact]
+    public void CompareWith_ForStraightFlush()
+    {
+        Hand hand = FixtureHands.StraightFlush;
+        Assert.Equal(Result.Tie, hand.CompareWith(FixtureHands.StraightFlush));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.FourOfAKind));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.FullHouse));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Flush));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Straight));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.ThreeOfAKind));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.TwoPairs));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Pair));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.HighCard));
+    }
+
+    [Fact]
+    public void CompareWith_ForFourOfAKind()
+    {
+        Hand hand = FixtureHands.FourOfAKind;
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.StraightFlush));
+        Assert.Equal(Result.Tie, hand.CompareWith(FixtureHands.FourOfAKind));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.FullHouse));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Flush));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Straight));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.ThreeOfAKind));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.TwoPairs));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Pair));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.HighCard));
+    }
+
+    [Fact]
+    public void CompareWith_ForFullHouse()
+    {
+        Hand hand = FixtureHands.FullHouse;
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.StraightFlush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FourOfAKind));
+        Assert.Equal(Result.Tie, hand.CompareWith(FixtureHands.FullHouse));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Flush));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Straight));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.ThreeOfAKind));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.TwoPairs));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Pair));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.HighCard));
+    }
+
+    [Fact]
+    public void CompareWith_ForFlush()
+    {
+        Hand hand = FixtureHands.Flush;
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.StraightFlush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FourOfAKind));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FullHouse));
+        Assert.Equal(Result.Tie, hand.CompareWith(FixtureHands.Flush));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Straight));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.ThreeOfAKind));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.TwoPairs));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Pair));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.HighCard));
+    }
+
+    [Fact]
+    public void CompareWith_ForStraight()
+    {
+        Hand hand = FixtureHands.Straight;
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.StraightFlush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FourOfAKind));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FullHouse));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Flush));
+        Assert.Equal(Result.Tie, hand.CompareWith(FixtureHands.Straight));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.ThreeOfAKind));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.TwoPairs));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Pair));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.HighCard));
+    }
+
+    [Fact]
+    public void CompareWith_ForThreeOfAKind()
+    {
+        Hand hand = FixtureHands.ThreeOfAKind;
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.StraightFlush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FourOfAKind));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FullHouse));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Flush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Straight));
+        Assert.Equal(Result.Tie, hand.CompareWith(FixtureHands.ThreeOfAKind));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.TwoPairs));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Pair));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.HighCard));
+    }
+
+    [Fact]
+    public void CompareWith_ForTwoPairs()
+    {
+        Hand hand = FixtureHands.TwoPairs;
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.StraightFlush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FourOfAKind));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FullHouse));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Flush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Straight));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.ThreeOfAKind));
+        Assert.Equal(Result.Tie, hand.CompareWith(FixtureHands.TwoPairs));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.Pair));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.HighCard));
+    }
+
+    [Fact]
+    public void CompareWith_ForPair()
+    {
+        Hand hand = FixtureHands.Pair;
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.StraightFlush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FourOfAKind));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FullHouse));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Flush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Straight));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.ThreeOfAKind));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.TwoPairs));
+        Assert.Equal(Result.Tie, hand.CompareWith(FixtureHands.Pair));
+        Assert.Equal(Result.Win, hand.CompareWith(FixtureHands.HighCard));
+    }
+
+    [Fact]
+    public void CompareWith_ForHighCard()
+    {
+        Hand hand = FixtureHands.HighCard;
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.StraightFlush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FourOfAKind));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.FullHouse));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Flush));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Straight));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.ThreeOfAKind));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.TwoPairs));
+        Assert.Equal(Result.Loss, hand.CompareWith(FixtureHands.Pair));
+        Assert.Equal(Result.Tie, hand.CompareWith(FixtureHands.HighCard));
+    }
+
+    #endregion
+
+
+    #region CompareWith Tests With Tiebreakers
+
+    [Theory]
+    [InlineData(CardValue.Ace, CardValue.Two, CardValue.Three, CardValue.Four, CardValue.Five)]
+    [InlineData(CardValue.Five, CardValue.Six, CardValue.Seven, CardValue.Eight, CardValue.Nine)]
+    public void CompareWith_StraightFlushWithTiebreaker(CardValue opp1, CardValue opp2, CardValue opp3, CardValue opp4, CardValue opp5)
+    {
+        Hand hand = new([
+            new(CardValue.Ace, CardSuit.Hearts),
+            new(CardValue.King, CardSuit.Hearts),
+            new(CardValue.Queen, CardSuit.Hearts),
+            new(CardValue.Jack, CardSuit.Hearts),
+            new(CardValue.Ten, CardSuit.Hearts)
+        ]);
+
+        Hand opponentHand = new([
+            new(opp1, CardSuit.Diamonds),
+            new(opp2, CardSuit.Diamonds),
+            new(opp3, CardSuit.Diamonds),
+            new(opp4, CardSuit.Diamonds),
+            new(opp5, CardSuit.Diamonds)
+        ]);
+
+        Assert.Equal(Result.Win, hand.CompareWith(opponentHand));
+    }
+
+    [Theory]
+    [InlineData(CardValue.Two, CardValue.Six, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Four, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Six, Result.Loss)]
+    public void CompareWith_FourOfAKindWithTiebreaker(CardValue oppQuad, CardValue oppSingle, Result expected)
+    {
+        Hand hand = new([
+            new(CardValue.Ace, CardSuit.Hearts),
+            new(CardValue.Ace, CardSuit.Diamonds),
+            new(CardValue.Ace, CardSuit.Clubs),
+            new(CardValue.Ace, CardSuit.Spades),
+            new(CardValue.Five, CardSuit.Hearts)
+        ]);
+
+        Hand opponentHand = new([
+            new(oppQuad, CardSuit.Hearts),
+            new(oppQuad, CardSuit.Diamonds),
+            new(oppQuad, CardSuit.Clubs),
+            new(oppQuad, CardSuit.Spades),
+            new(oppSingle, CardSuit.Hearts)
+        ]);
+
+        Assert.Equal(expected, hand.CompareWith(opponentHand));
+    }
+
+    [Theory]
+    [InlineData(CardValue.Two, CardValue.Six, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Four, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Six, Result.Loss)]
+    public void CompareWith_FullHouseWithTiebreaker(CardValue oppTriple, CardValue oppDouble, Result expected)
+    {
+        Hand hand = new([
+            new(CardValue.Ace, CardSuit.Hearts),
+            new(CardValue.Ace, CardSuit.Diamonds),
+            new(CardValue.Ace, CardSuit.Clubs),
+            new(CardValue.Five, CardSuit.Spades),
+            new(CardValue.Five, CardSuit.Hearts)
+        ]);
+
+        Hand opponentHand = new([
+            new(oppTriple, CardSuit.Hearts),
+            new(oppTriple, CardSuit.Diamonds),
+            new(oppTriple, CardSuit.Clubs),
+            new(oppDouble, CardSuit.Spades),
+            new(oppDouble, CardSuit.Hearts)
+        ]);
+
+        Assert.Equal(expected, hand.CompareWith(opponentHand));
+    }
+
+    [Theory]
+    [InlineData(CardValue.Queen, CardValue.Six, CardValue.Five, CardValue.Three, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Six, CardValue.Five, CardValue.Three, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Ten, CardValue.Five, CardValue.Three, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Ten, CardValue.Seven, CardValue.Three, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Ten, CardValue.Seven, CardValue.Four, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.King, CardValue.Seven, CardValue.Three, CardValue.Two, Result.Loss)]
+    public void CompareWith_FlushWithTiebreaker(CardValue opp1, CardValue opp2, CardValue opp3, CardValue opp4, CardValue opp5, Result expected)
+    {
+        Hand hand = new([
+            new(CardValue.Ace, CardSuit.Hearts),
+            new(CardValue.Ten, CardSuit.Hearts),
+            new(CardValue.Seven, CardSuit.Hearts),
+            new(CardValue.Four, CardSuit.Hearts),
+            new(CardValue.Three, CardSuit.Hearts)
+        ]);
+
+        Hand opponentHand = new([
+            new(opp1, CardSuit.Diamonds),
+            new(opp2, CardSuit.Diamonds),
+            new(opp3, CardSuit.Diamonds),
+            new(opp4, CardSuit.Diamonds),
+            new(opp5, CardSuit.Diamonds)
+        ]);
+
+        Assert.Equal(expected, hand.CompareWith(opponentHand));
+    }
+
+    [Theory]
+    [InlineData(CardValue.Ace, CardValue.Two, CardValue.Three, CardValue.Four, CardValue.Five)]
+    [InlineData(CardValue.Five, CardValue.Six, CardValue.Seven, CardValue.Eight, CardValue.Nine)]
+    public void CompareWith_StraightWithTiebreaker(CardValue opp1, CardValue opp2, CardValue opp3, CardValue opp4, CardValue opp5)
+    {
+        Hand hand = new([
+            new(CardValue.Ace, CardSuit.Hearts),
+            new(CardValue.King, CardSuit.Clubs),
+            new(CardValue.Queen, CardSuit.Diamonds),
+            new(CardValue.Jack, CardSuit.Spades),
+            new(CardValue.Ten, CardSuit.Hearts)
+        ]);
+
+        Hand opponentHand = new([
+            new(opp1, CardSuit.Hearts),
+            new(opp2, CardSuit.Clubs),
+            new(opp3, CardSuit.Diamonds),
+            new(opp4, CardSuit.Spades),
+            new(opp5, CardSuit.Diamonds)
+        ]);
+
+        Assert.Equal(Result.Win, hand.CompareWith(opponentHand));
+    }
+
+    [Theory]
+    [InlineData(CardValue.Queen, CardValue.Six, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Six, CardValue.Two,  Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Seven, CardValue.Two,  Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Ten, CardValue.Seven,  Result.Loss)]
+    [InlineData(CardValue.Ace, CardValue.Seven, CardValue.Four,  Result.Loss)]
+    public void CompareWith_ThreeOfAKindWithTiebreaker(CardValue oppTriple, CardValue oppSingle1, CardValue oppSingle2, Result expected)
+    {
+        Hand hand = new([
+            new(CardValue.Ace, CardSuit.Hearts),
+            new(CardValue.Ace, CardSuit.Diamonds),
+            new(CardValue.Ace, CardSuit.Spades),
+            new(CardValue.Seven, CardSuit.Hearts),
+            new(CardValue.Three, CardSuit.Diamonds)
+        ]);
+
+        Hand opponentHand = new([
+            new(oppTriple, CardSuit.Hearts),
+            new(oppTriple, CardSuit.Diamonds),
+            new(oppTriple, CardSuit.Spades),
+            new(oppSingle1, CardSuit.Hearts),
+            new(oppSingle2, CardSuit.Diamonds)
+        ]);
+
+        Assert.Equal(expected, hand.CompareWith(opponentHand));
+    }
+
+    [Theory]
+    [InlineData(CardValue.King, CardValue.Ten, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Ten, CardValue.Two,  Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Jack, CardValue.Two,  Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Queen, CardValue.Seven,  Result.Loss)]
+    [InlineData(CardValue.Ace, CardValue.Jack, CardValue.Four,  Result.Loss)]
+    public void CompareWith_TwoPairsWithTiebreaker(CardValue oppPair1, CardValue oppPair2, CardValue oppSingle, Result expected)
+    {
+        Hand hand = new([
+            new(CardValue.Ace, CardSuit.Hearts),
+            new(CardValue.Ace, CardSuit.Diamonds),
+            new(CardValue.Jack, CardSuit.Spades),
+            new(CardValue.Jack, CardSuit.Hearts),
+            new(CardValue.Three, CardSuit.Diamonds)
+        ]);
+
+        Hand opponentHand = new([
+            new(oppPair1, CardSuit.Hearts),
+            new(oppPair1, CardSuit.Diamonds),
+            new(oppPair2, CardSuit.Spades),
+            new(oppPair2, CardSuit.Hearts),
+            new(oppSingle, CardSuit.Diamonds)
+        ]);
+
+        Assert.Equal(expected, hand.CompareWith(opponentHand));
+    }
+
+    [Theory]
+    [InlineData(CardValue.King, CardValue.Ten, CardValue.Six, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Ten, CardValue.Six, CardValue.Two,  Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Jack, CardValue.Six, CardValue.Two,  Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Jack, CardValue.Seven, CardValue.Two,  Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Queen, CardValue.Six, CardValue.Two,  Result.Loss)]
+    [InlineData(CardValue.Ace, CardValue.Jack, CardValue.Nine, CardValue.Four,  Result.Loss)]
+    public void CompareWith_PairWithTiebreaker(CardValue oppPair, CardValue oppSingle1, CardValue oppSingle2, CardValue oppSingle3, Result expected)
+    {
+        Hand hand = new([
+            new(CardValue.Ace, CardSuit.Hearts),
+            new(CardValue.Ace, CardSuit.Diamonds),
+            new(CardValue.Jack, CardSuit.Spades),
+            new(CardValue.Seven, CardSuit.Hearts),
+            new(CardValue.Three, CardSuit.Diamonds)
+        ]);
+
+        Hand opponentHand = new([
+            new(oppPair, CardSuit.Hearts),
+            new(oppPair, CardSuit.Diamonds),
+            new(oppSingle1, CardSuit.Spades),
+            new(oppSingle2, CardSuit.Hearts),
+            new(oppSingle3, CardSuit.Diamonds)
+        ]);
+
+        Assert.Equal(expected, hand.CompareWith(opponentHand));
+    }
+
+    [Theory]
+    [InlineData(CardValue.Queen, CardValue.Six, CardValue.Five, CardValue.Three, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Six, CardValue.Five, CardValue.Three, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Ten, CardValue.Five, CardValue.Three, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Ten, CardValue.Seven, CardValue.Three, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.Ten, CardValue.Seven, CardValue.Four, CardValue.Two, Result.Win)]
+    [InlineData(CardValue.Ace, CardValue.King, CardValue.Seven, CardValue.Three, CardValue.Two, Result.Loss)]
+    public void CompareWith_HighCardWithTiebreaker(CardValue opp1, CardValue opp2, CardValue opp3, CardValue opp4, CardValue opp5, Result expected)
+    {
+        Hand hand = new([
+            new(CardValue.Ace, CardSuit.Hearts),
+            new(CardValue.Ten, CardSuit.Diamonds),
+            new(CardValue.Seven, CardSuit.Clubs),
+            new(CardValue.Four, CardSuit.Spades),
+            new(CardValue.Three, CardSuit.Hearts)
+        ]);
+
+        Hand opponentHand = new([
+            new(opp1, CardSuit.Diamonds),
+            new(opp2, CardSuit.Hearts),
+            new(opp3, CardSuit.Clubs),
+            new(opp4, CardSuit.Spades),
+            new(opp5, CardSuit.Diamonds)
+        ]);
+
+        Assert.Equal(expected, hand.CompareWith(opponentHand));
+    }
+
+
+
+    #endregion
 }
